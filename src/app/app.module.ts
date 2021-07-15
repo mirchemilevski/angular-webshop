0aff22c7c4cf3cc83d94ac34b36a28febd2bb64c
Mirche Milevski <mirche.milevski@yahoo.com>
2021-07-15 10:24:03 +0200

    added starter files, components architecture

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CarouselPageComponent } from './components/carousel-page/carousel-page.component';
+import { StorePageComponent } from './components/store-page/store-page.component';
+import { NavbarComponent } from './components/carousel-page/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel-page/carousel/carousel.component';
+import { CartComponent } from './components/carousel-page/navbar/cart/cart.component';
+import { MenuBarComponent } from './components/store-page/menu-bar/menu-bar.component';
+import { StoreComponent } from './components/store-page/store/store.component';
+import { FooterComponent } from './components/store-page/footer/footer.component';
+import { FiltersComponent } from './components/store-page/store/filters/filters.component';
+import { ProductsComponent } from './components/store-page/store/products/products.component';
+import { ProductComponent } from './components/store-page/store/products/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselPageComponent,
+    StorePageComponent,
+    NavbarComponent,
+    CarouselComponent,
+    CartComponent,
+    MenuBarComponent,
+    StoreComponent,
+    FooterComponent,
+    FiltersComponent,
+    ProductsComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/carousel-page/carousel-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarouselPageComponent } from './carousel-page.component';
+
+describe('CarouselPageComponent', () => {
+  let component: CarouselPageComponent;
+  let fixture: ComponentFixture<CarouselPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarouselPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarouselPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
